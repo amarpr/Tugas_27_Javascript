5fef3a4434697780a9a2d415c0e81b312abbf26d
Muhamar <amararrasyid@gmail.com>
2020-05-12 18:39:53 +0700

    Tugas 27 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,17 @@
+function tugas() {
+    var a = "No ";
+    var b = "rumah saya ";
+    var c = 30;
+    var d = "A";
+
+    var hasil1 = a + b + c + d;
+    console.log(hasil1);
+
+    var jln = "Jalan pangeran Tirtayasa No ";
+    var no = 70;
+
+    var hasil2 = jln + no;
+    console.log(hasil2);
+}
+
+tugas()
